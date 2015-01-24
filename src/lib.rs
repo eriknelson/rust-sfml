bbd5cb8eafcf656f23ed6b66274f8392c0138025
Erik Nelson <erik@nelsk.com>
2015-01-24 17:10:57 -0500

    Trying to quiet some of these warnings to no avail...

--- src/lib.rs
+++ src/lib.rs
@@ -124,9 +124,14 @@
 #![doc(html_logo_url = "http://rust-sfml.org/logo_rsfml.png")]
 #![crate_type = "rlib"]
 #![crate_type = "dylib"]
-#![allow(dead_code, non_camel_case_types, unused_attributes)]
-#![warn(missing_docs)]
+#![allow(dead_code, non_camel_case_types, unused_attributes, unstable)]
+
+//////////////////////////////
+// NOTE: ERIK MODS
+//#![warn(missing_docs)]
 #![feature(unsafe_destructor)]
+#![feature(int_uint)]
+//////////////////////////////
 
 extern crate libc;
 extern crate core;
